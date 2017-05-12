9e37f67c5891881ac15bf160b9b2c54223fae2ba
EpicHound <528938@hull.ac.uk>
2017-05-12 19:29:05 +0100

    Fixed ignoring

---
+++ Game/Assets/Scripts/StateManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+    State currentState;
+	// Use this for initialization
+	void Start () {
+        currentState = State.Idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public State GetState()
+    {
+        return currentState;
+    }
+    public enum State
+    {
+        Idle,
+        Aiming,
+        Shooting
+    }
+
+}
---
+++ Game/Assets/Scripts/dragonAi.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonAi : MonoBehaviour {
+    // Use this for initialization
+   public StateManager stateManager;
+    public float smooth = 1f;
+    public float angle = 90;
+    public float desiredRotation;
+    private Quaternion initialRotation;
+    public float timeToLerp = 1f;
+    void Start () {
+        initialRotation = transform.rotation;
+        desiredRotation = angle;
+        desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRotation);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+
+    private void FixedUpdate()
+    {
+        
+        Aiming();
+    }
+    Quaternion desiredRotQ;
+    void Aiming()
+    {
+        HeadRotate();
+
+        
+           
+        
+        
+        
+        
+    }
+    float StartTime;
+    void HeadRotate()
+    {
+
+        float timeSinceStart = Time.time - StartTime;
+        float percentComplete = timeSinceStart / timeToLerp;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, percentComplete);
+        if (percentComplete > 1.0f)
+        {
+            StartTime = Time.time;
+            desiredRotation = -1 * desiredRotation;
+            desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRotation);
+        }
+    }
+
+}
