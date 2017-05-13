f9c16702027f70d9d92de12edcc89b0bd44a620b
AmazingSock <529709@hull.ac.uk>
2017-05-13 04:47:14 +0100

    Added Splash Screen

---
+++ Game/Assets/Scripts/Fire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fire : MonoBehaviour {
+    public ParticleSystem FireEffect;
+    private float TimeEffect;
+	void Start ()
+    {
+		
+	}
+	void Update ()
+    {
+        FireEffect.gameObject.SetActive(true);
+        TimeEffect += Time.deltaTime;
+        if (TimeEffect > 5)
+        {
+            SceneManager.LoadScene("JetpackLevel");
+        }
+	}
+}
