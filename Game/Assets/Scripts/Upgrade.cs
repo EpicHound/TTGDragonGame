bdd432724560b0bdac43826e2847528d2b7267b2
AmazingSock <529709@hull.ac.uk>
2017-05-13 14:03:21 +0100

    Shop Stuff

---
+++ Game/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Upgrade : MonoBehaviour {
+    int CurrentCoins = 0;
+    public Text Text;
+	// Use this for initialization
+	void Start ()
+    {
+        CurrentCoins = PlayerPrefs.GetInt("Coins", 19);
+        Text.text = ("You Have: " + CurrentCoins + " Coins."); 	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerPrefs.SetInt("Coins", CurrentCoins);
+        Text.text = ("You Have: " + CurrentCoins + " Coins.");
+    }
+}
