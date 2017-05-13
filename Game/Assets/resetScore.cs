ab5ea7ffee7e218208d5b6dfb5938cfbe8b60f1f
EpicHound <528938@hull.ac.uk>
2017-05-13 14:06:04 +0100

    ..

--- Game/Assets/resetScore.cs
+++ Game/Assets/resetScore.cs
@@ -6,8 +6,8 @@ public class resetScore : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        PlayerPrefs.SetInt("CurrentScore", 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
