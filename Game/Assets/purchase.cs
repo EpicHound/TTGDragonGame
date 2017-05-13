161ed4157b1890a7c5d152970278e5cff6288eef
EpicHound <528938@hull.ac.uk>
2017-05-13 12:44:46 +0100

    Added skins

---
+++ Game/Assets/purchase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class purchase : MonoBehaviour {
+    public int skinNumber;
+    public int Price;
+    bool isBought = false;
+    public Sprite switchSprite;
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.GetInt("skin" + skinNumber + "isBought") == 1)
+        {
+            GetComponent<SpriteRenderer>().sprite = switchSprite;
+            isBought = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if(isBought == true)
+        {
+            PlayerPrefs.SetInt("Selected Skin", skinNumber);
+            PlayerPrefs.SetInt("skin" + skinNumber + "isBought", 1);
+        }
+        else
+        {
+            if(PlayerPrefs.GetInt("Coins") >= Price)
+            {
+                Price -= PlayerPrefs.GetInt("Coins");
+                PlayerPrefs.SetInt("Selected Skin", skinNumber);
+                PlayerPrefs.SetInt("skin" + skinNumber + "isBought", 1);
+            }
+        }
+    }
+}
